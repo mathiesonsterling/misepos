497ad77ee4752641811b3361b9a2c007b4b9e860
Mathieson Sterling <mathieson@misepos.com>
2015-07-19 15:26:07 -0400

    INVENTORY-254 started adding standard sizes

--- Mise.Core/ValueItems/Inventory/LiquidContainer.cs
+++ Mise.Core/ValueItems/Inventory/LiquidContainer.cs
@@ -6,19 +6,133 @@ using System.Threading.Tasks;
 
 namespace Mise.Core.ValueItems.Inventory
 {
-	public class LiquidContainer : IEquatable<LiquidContainer>, ITextSearchable
+    public class LiquidContainer : IEquatable<LiquidContainer>, ITextSearchable
     {
-		private string _displayName;
-		public string DisplayName{get{
-				if(string.IsNullOrEmpty (_displayName)){
-					return AmountContained.GetInMilliliters () + " ml";
-				}
-				return _displayName;
-			}
-			set{
-				_displayName = value;
-			}
-		}
+        #region Standard sizes
+
+        public static LiquidContainer Bottle750ML
+        {
+            get { return new LiquidContainer { AmountContained = new LiquidAmount { Milliliters = 750 }, DisplayName = "750ml Bottle" }; }
+        }
+
+        public static LiquidContainer Bottle1L
+        {
+            get { return new LiquidContainer { AmountContained = new LiquidAmount { Milliliters = 1000 }, DisplayName = "1L Bottle" }; }
+        }
+
+        public static LiquidContainer Bottle375ML
+        {
+            get { return new LiquidContainer { AmountContained = new LiquidAmount { Milliliters = 375 }, DisplayName = "375ml Bottle" }; }
+        }
+
+        public static LiquidContainer Can12oz
+        {
+            get { return new LiquidContainer { AmountContained = LiquidAmount.FromLiquidOunces(12.0M), DisplayName = "12oz Can" }; }
+        }
+
+        public static LiquidContainer Can16oz
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = LiquidAmount.FromLiquidOunces(16.0M),
+                    DisplayName = "16oz Can"
+                };
+            }
+        }
+
+        public static LiquidContainer Can10oz
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = LiquidAmount.FromLiquidOunces(10.0M),
+                    DisplayName = "10oz Bottle"
+                };
+            }
+        }
+        public static LiquidContainer Bottle12oz
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = LiquidAmount.FromLiquidOunces(12.0M),
+                    DisplayName = "12oz Bottle"
+                };
+            }
+        }
+
+        public static LiquidContainer Bottle7oz
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = LiquidAmount.FromLiquidOunces(7.0M),
+                    DisplayName = "7oz Bottle"
+                };
+            }
+        }
+
+        public static LiquidContainer Bottle330ml
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = new LiquidAmount { Milliliters = 330 },
+                    DisplayName = "330ml Bottle"
+                };
+            }
+        }
+
+        public static LiquidContainer Bottle500ml
+        {
+            get
+            {
+                return new LiquidContainer
+                {
+                    AmountContained = new LiquidAmount { Milliliters = 500 },
+                    DisplayName = "500ml Bottle"
+                };
+            }
+        }
+        public static IEnumerable<LiquidContainer> GetStandardBarSizes()
+        {
+            return new List<LiquidContainer>
+            {
+                Bottle750ML,
+                Bottle375ML,
+                Bottle330ml,
+                Bottle1L,
+                Bottle500ml,
+                Bottle1L,
+                Bottle12oz,
+                Can12oz,
+                Bottle7oz,
+            };
+        }
+        #endregion
+
+        private string _displayName;
+        public string DisplayName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_displayName))
+                {
+                    return AmountContained.GetInMilliliters() + " ml";
+                }
+                return _displayName;
+            }
+            set
+            {
+                _displayName = value;
+            }
+        }
 
         /// <summary>
         /// The total amount of liquid this container can hold
@@ -37,18 +151,20 @@ namespace Mise.Core.ValueItems.Inventory
 
         public bool Equals(LiquidContainer other)
         {
-			if(string.IsNullOrEmpty (_displayName) == false){
-				if(_displayName != other.DisplayName){
-					return false;
-				}
-			}
+            if (string.IsNullOrEmpty(_displayName) == false)
+            {
+                if (_displayName != other.DisplayName)
+                {
+                    return false;
+                }
+            }
 
             if (AmountContained.Equals(other.AmountContained) == false)
             {
                 return false;
             }
             if ((WeightEmpty == null && other.WeightEmpty != null)
-                ||( WeightEmpty != null && other.WeightEmpty == null))
+                || (WeightEmpty != null && other.WeightEmpty == null))
             {
                 return false;
             }
@@ -92,28 +208,28 @@ namespace Mise.Core.ValueItems.Inventory
             }
         }
 
-	    public class ContainerComparer : IEqualityComparer<LiquidContainer>
-	    {
-	        public bool Equals(LiquidContainer x, LiquidContainer y)
-	        {
-	            return x.Equals(y);
-	        }
+        public class ContainerComparer : IEqualityComparer<LiquidContainer>
+        {
+            public bool Equals(LiquidContainer x, LiquidContainer y)
+            {
+                return x.Equals(y);
+            }
 
-	        public int GetHashCode(LiquidContainer lc)
-	        {
-	            return lc.GetHashCode();
-	        }
-	    }
+            public int GetHashCode(LiquidContainer lc)
+            {
+                return lc.GetHashCode();
+            }
+        }
 
-		#region ITextSearchable implementation
+        #region ITextSearchable implementation
 
-		public bool ContainsSearchString (string searchString)
-		{
-			return (AmountContained != null && AmountContained.Milliliters.ToString ().Contains (searchString)) 
+        public bool ContainsSearchString(string searchString)
+        {
+            return (AmountContained != null && AmountContained.Milliliters.ToString().Contains(searchString))
                 || (DisplayName != null && DisplayName.ToUpper().Contains(searchString.ToUpper()));
-		}
+        }
 
-		#endregion
+        #endregion
     }
 
 
