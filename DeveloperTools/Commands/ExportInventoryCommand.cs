7e71507b34ce5b296c2d1ac83fbd0e912377a748
Mathieson Sterling <mathieson@misepos.com>
2015-08-09 10:59:59 -0400

    Added ability to export inventory to CSV using developer tools

---
+++ DeveloperTools/Commands/ExportInventoryCommand.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Mise.Core.Common.Entities;
+using Mise.Core.Common.Entities.DTOs;
+using Mise.Core.Common.Entities.DTOs.AzureTypes;
+using Mise.Core.Common.Entities.Inventory;
+using Mise.Core.Common.Services.Implementation.Serialization;
+using Mise.Core.Services.UtilityServices;
+using Mise.VendorManagement.Services.Implementation;
+
+namespace DeveloperTools.Commands
+{
+    public class ExportInventoryCommand : BaseProgressReportingCommand
+    {
+        private readonly ILogger _logger;
+        private readonly IMobileServiceClient _client;
+        private readonly EntityDataTransportObjectFactory _entityDataTransportObjectFactory;
+        public ExportInventoryCommand(IProgress<ProgressReport> progress, ILogger logger) : base(progress)
+        {
+            _logger = logger;
+
+            _client = new MobileServiceClient(
+                "https://stockboymobileservice.azure-mobile.net/",
+                "vvECpsmISLzAxntFjNgSxiZEPmQLLG42"
+            );
+
+            _entityDataTransportObjectFactory = new EntityDataTransportObjectFactory(new JsonNetSerializer());
+        }
+
+        public override async Task Execute()
+        {
+            Report("Getting last inventory from DB . . . ");
+            var invType = typeof (Inventory).ToString();
+
+            var table = _client.GetTable<AzureEntityStorage>();
+
+            Report("Getting inventories . . . . ");
+            var inventories = await table
+                .Where(ai => ai.MiseEntityType == invType)
+                .OrderByDescending(ai => ai.LastUpdatedDate)
+                .ToEnumerableAsync();
+
+            var lastInvAI = inventories.FirstOrDefault();
+
+            Report("Transforming from JSON to inventories");
+            var lastInv = _entityDataTransportObjectFactory.FromDataStorageObject<Inventory>(lastInvAI.ToRestaurantDTO());
+
+            Report("Getting restaurant for inventory");
+
+            var restType = typeof (Restaurant).ToString();
+            var restAIs = await table.Where(ai => ai.MiseEntityType == restType)
+                .Where(ai => ai.EntityID == lastInv.RestaurantID)
+                .ToEnumerableAsync();
+
+            var restAI = restAIs.FirstOrDefault();
+           
+
+            string fileName = "UnknownRestInv__" + lastInv.CreatedDate.ToString("d") + ".csv";
+            if (restAI != null)
+            {
+                var rest = _entityDataTransportObjectFactory.FromDataStorageObject<Restaurant>(restAI.ToRestaurantDTO());
+                fileName = rest.Name.ShortName+"__"+ lastInv.CreatedDate.ToString("d") + ".csv";
+            }
+            fileName = fileName.Replace("/", "_");
+
+            Report("Exporting items to CSV");
+            var service = new InventoryCSVExportService(_logger);
+
+            await service.ExportInventoryToCsvFile(fileName, lastInv);
+
+            Finish();
+        }
+
+        public override int NumSteps
+        {
+            get { return 5; }
+        }
+    }
+}
---
+++ Server/Mise.VendorManagement/Services/Implementation/InventoryCSVExportService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using Mise.Core.Entities.Inventory;
+using Mise.Core.Services.UtilityServices;
+
+namespace Mise.VendorManagement.Services.Implementation
+{
+    public class InventoryCSVExportService : IInventoryExportService
+    {
+        public readonly ILogger _logger;
+
+        public InventoryCSVExportService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task ExportInventoryToCsvFile(string filename, IInventory inventory)
+        {
+            return Task.Run(() =>
+            {
+                var lineItems = inventory.GetBeverageLineItems().ToList();
+                if (lineItems.Any() == false)
+                {
+                    throw new ArgumentException("No line items in the given inventory!");
+                }
+
+                var writer = GetTextWriter(filename);
+                using (writer)
+                {
+                    var csv = new CsvWriter(writer);
+
+                    foreach (var li in lineItems)
+                    {
+                        csv.WriteField(li.DisplayName);
+                        csv.WriteField(li.Container.DisplayName);
+
+
+                        var numpartials = li.PartialBottlePercentages.Any()
+                            ? li.PartialBottlePercentages.Sum(p => p)
+                            : 0;
+
+                        csv.WriteField(li.Quantity);
+
+                        csv.WriteField(li.CurrentAmount.Milliliters);
+                        csv.WriteField(li.NumFullBottles);
+
+                        csv.WriteField(numpartials);
+
+                        csv.NextRecord();
+                    }
+
+                    csv.WriteRecords(lineItems);
+                }
+            });
+        }
+
+        private static TextWriter GetTextWriter(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            return File.CreateText(filename);
+        }
+    }
+}
