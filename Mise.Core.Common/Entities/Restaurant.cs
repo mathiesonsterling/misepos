027684d1e45ee94c77a611d33f679697487f33a9
Mathieson Sterling <mathieson@misepos.com>
2015-12-29 00:46:20 -0500

    Cleaned up restaurant object for fields not needed for Stockboy

--- Mise.Core.Common/Entities/Restaurant.cs
+++ Mise.Core.Common/Entities/Restaurant.cs
@@ -17,10 +17,6 @@ namespace Mise.Core.Common.Entities
     {
         public Restaurant()
         {
-            DiscountAmounts = new List<DiscountAmount>();
-            DiscountPercentages = new List<DiscountPercentage>();
-            DiscountPercentageAfterMinimumCashTotals = new List<DiscountPercentageAfterMinimumCashTotal>();
-            Terminals = new List<MiseTerminalDevice>();
             InventorySections = new List<RestaurantInventorySection>();
         }
 
@@ -28,19 +24,11 @@ namespace Mise.Core.Common.Entities
         {
             var newItem = base.CloneRestaurantBase(new Restaurant());
             newItem.AccountID = AccountID;
-            newItem.FriendlyID = FriendlyID;
             newItem.InventorySections = InventorySections.Select(i => i.Clone() as RestaurantInventorySection).ToList();
             newItem.Name = Name;
-            newItem.NumberOfActiveCashRegisters = NumberOfActiveCashRegisters;
-            newItem.NumberOfActiveCreditRegisters = NumberOfActiveCreditRegisters;
-            newItem.NumberOfActiveOrderTerminals = NumberOfActiveOrderTerminals;
             newItem.PhoneNumber = PhoneNumber;
-            newItem.RestaurantServerLocation = RestaurantServerLocation;
             newItem.StreetAddress = StreetAddress;
-            newItem.Terminals = Terminals;
-            newItem.LastMeasuredInventoryID = LastMeasuredInventoryID;
-            newItem.CurrentInventoryID = CurrentInventoryID;
-           
+
             return newItem;
         }
 
@@ -48,32 +36,8 @@ namespace Mise.Core.Common.Entities
 			get;
 			set;
 		}
-
-        public string FriendlyID { get; set; }
-
-        public List<MiseTerminalDevice> Terminals { get; set; }
-
-        public IEnumerable<IMiseTerminalDevice> GetTerminals()
-        {
-            return Terminals;
-        }
-
-        public void AddTerminal(IMiseTerminalDevice device)
-        {
-            var concrete = device as MiseTerminalDevice;
-            if (concrete != null)
-            {
-                Terminals.Add(concrete);
-            }
-        }
-
-        public Uri RestaurantServerLocation { get; set; }
-
-
+            
         public List<RestaurantInventorySection> InventorySections { get; set; }
-
-
-        public Guid? LastMeasuredInventoryID { get; set; }
 
         public IEnumerable<IRestaurantInventorySection> GetInventorySections()
         {
@@ -91,29 +55,15 @@ namespace Mise.Core.Common.Entities
 			set;
 		}
 
-        public List<DiscountAmount> DiscountAmounts;
-        public List<DiscountPercentage> DiscountPercentages;
-        public List<DiscountPercentageAfterMinimumCashTotal> DiscountPercentageAfterMinimumCashTotals;
-
-		public IEnumerable<IDiscount> GetPossibleDiscounts(){
-            var list = new List<IDiscount>();
-            list.AddRange(DiscountAmounts);
-            list.AddRange(DiscountPercentages);
-            list.AddRange(DiscountPercentageAfterMinimumCashTotals);
-
-            return list;
-		}
- 
-        public int NumberOfActiveCashRegisters { get; set; }
-        public int NumberOfActiveCreditRegisters { get; set; }
-        public int NumberOfActiveOrderTerminals { get; set; }
+        public List<EmailAddress> EmailsToSendInventoryReportsTo{ get; set; }
+        public IEnumerable<EmailAddress> GetEmailsToSendInventoryReportsTo(){
+            return EmailsToSendInventoryReportsTo;
+        }
 
         /// <summary>
         /// If this is true, this is just here to support a user, but is not verified as a real restaurant
         /// </summary>
         public bool IsPlaceholder { get { return AccountID.HasValue == false; } }
-
-        public Guid? CurrentInventoryID { get; set; }
 
         public void When(IRestaurantEvent entityEvent)
         {
--- Mise.Core/Entities/Restaurant/IRestaurant.cs
+++ Mise.Core/Entities/Restaurant/IRestaurant.cs
@@ -29,56 +29,16 @@ namespace Mise.Core.Entities.Restaurant
 		PhoneNumber PhoneNumber{get;}
 
         /// <summary>
-        /// Gets the discounts or gratuities this restaurant has
-        /// </summary>
-        /// <value>The possible discounts.</value>
-        IEnumerable<IDiscount> GetPossibleDiscounts();
-
-        /// <summary>
-        /// How many registers (able to take cash payments) are up right now
-        /// </summary>
-        int NumberOfActiveCashRegisters { get; }
-
-        /// <summary>
-        /// How many registers (able to take credit payments) are up right now
-        /// </summary>
-        int NumberOfActiveCreditRegisters { get; }
-
-        /// <summary>
-        /// How many terminals, that can take orders, are currently up
-        /// </summary>
-        int NumberOfActiveOrderTerminals { get; }
-
-        /// <summary>
-        /// Unique, but friendly ID to be used for web lookups
-        /// </summary>
-        string FriendlyID { get; }
-
-        IEnumerable<IMiseTerminalDevice> GetTerminals();
-        void AddTerminal(IMiseTerminalDevice device);
-
-        Uri RestaurantServerLocation { get; }
-
-        /// <summary>
         /// If this is true, this is just here to support a user, but is not verified as a real restaurant
         /// </summary>
         bool IsPlaceholder { get;}
-
-        /// <summary>
-        /// Gets the current estimated inventory
-        /// </summary>
-        /// <returns></returns>
-        Guid? CurrentInventoryID { get; }
-        /// <summary>
-        /// Gets the last time we measured inventory
-        /// </summary>
-        /// <returns></returns>
-        Guid? LastMeasuredInventoryID { get; }
 
         /// <summary>
         /// Get any inventory sections that have been setup
         /// </summary>
         /// <returns></returns>
         IEnumerable<IRestaurantInventorySection> GetInventorySections();
+
+        IEnumerable<EmailAddress> GetEmailsToSendInventoryReportsTo();
     }
 }
