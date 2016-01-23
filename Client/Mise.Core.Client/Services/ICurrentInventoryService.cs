6ea09a7d8068b9705adebba0b3631fb709622113
Mathieson Sterling <mathieson@misepos.com>
2016-01-23 12:12:47 -0500

    Added current inventory service for BlackBook.

---
+++ Client/Mise.Core.Client/Services/ICurrentInventoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mise.Core.ValueItems.Inventory;
+using Mise.Core.Entities.Inventory;
+namespace Services
+{
+    /// <summary>
+    /// Reports on the amount of items we believe currently in a restaurant
+    /// </summary>
+    public interface ICurrentInventoryService
+    {
+        /// <summary>
+        /// If we thing we currently have an item in the restaurant
+        /// </summary>
+        /// <param name="lineItem">Line item.</param>
+        Task<bool> IsItemCurrentlyInStock(IBaseBeverageLineItem lineItem);
+    }
+}
+
