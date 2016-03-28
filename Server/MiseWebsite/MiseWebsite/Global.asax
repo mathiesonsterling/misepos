1c43439197740b2118749c0c9304f2e00b1b6114
Mathieson Sterling <mathieson@misepos.com>
2016-03-27 21:58:32 -0400

    Added MiseWebsite project to eventually become main website

---
+++ Server/MiseWebsite/MiseWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiseWebsite.MvcApplication" Language="C#" %>
---
+++ Server/MiseWebsite/MiseWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiseWebsite.Startup))]
+namespace MiseWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
