35315df5e1d72d6f356c44de5aaa9ad413c55dd8
Mathieson Sterling <mathieson@misepos.com>
2016-04-23 22:09:17 -0400

    Logoff, forms auth in restaurant controller

--- Server/MiseWebsite/MiseWebsite/Controllers/AccountController.cs
+++ Server/MiseWebsite/MiseWebsite/Controllers/AccountController.cs
@@ -188,7 +188,7 @@ namespace MiseWebsite.Controllers
                 if (result.Succeeded)
                 { 
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
