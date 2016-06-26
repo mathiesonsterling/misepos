c3005a1c94fcc24fcb5bfdebded1fd3fe3036849
Mathieson Sterling <mathieson@misepos.com>
2016-06-26 00:23:31 -0400

    More transfer

---
+++ Client/Mise.Inventory/Mise.Inventory/Mise.Inventory.iOS/Services/NoProcessorService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mise.Core.Entities.Payments;
+using Mise.Core.Services;
+using Mise.Core.ValueItems;
+
+namespace Mise.Inventory.iOS.Services
+{
+    public class NoProcessorService : ICreditCardProcessorService
+    {
+        public void AuthorizeCard (ICreditCardPayment payment, Action<ICreditCardPayment, CreditCardAuthorizationCode> authCallback)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void ChargeCard (ICreditCardPayment payment, CreditCardAuthorizationCode authFromPrevious, Action<ICreditCardPayment, CreditCardAuthorizationCode> closeCallback)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<CreditCard> GetCardAfterAuthorization (string paymentID)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<CreditCard> SendCardToProcessorForSubscription (PersonName cardName, CreditCardNumber number)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<string> SetPaymentID (Guid accountID, PersonName name, Money authorizationAmount)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void VoidAuthorization (ICreditCardPayment payment, CreditCardAuthorizationCode authFromPrevious, Action<ICreditCardPayment, CreditCardAuthorizationCode> closeCallback)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
+
--- Client/Mise.Inventory/Mise.Inventory/Mise.Inventory/Pages/UpdateParQuantityPage.xaml.cs
+++ Client/Mise.Inventory/Mise.Inventory/Mise.Inventory/Pages/UpdateParQuantityPage.xaml.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
-
+﻿using System.Collections.Generic;
 using Mise.Inventory.ViewModels;
-namespace Mise.Inventory
+using Xamarin;
+
+namespace Mise.Inventory.Pages
 {
-	public partial class UpdateParQuantityPage : ContentPage
+	public partial class UpdateParQuantityPage
 	{
 		public UpdateParQuantityPage ()
 		{
-			var vm = App.UpdateParQuantityViewModel;
+			var vm = App.UpdateParLineItemViewModel;
 			BindingContext = vm;
 			InitializeComponent ();
 		}
 
-		protected override async void OnAppearing ()
+	    public override BaseViewModel ViewModel => App.UpdateParLineItemViewModel;
+	    public override string PageName => "Update Par";
+
+	    protected override async void OnAppearing ()
 		{
-			Xamarin.Insights.Track("ScreenLoaded", new Dictionary<string, string>{{"ScreenName", "UpdateParQuantityPage"}});
+			Insights.Track("ScreenLoaded", new Dictionary<string, string>{{"ScreenName", "UpdateParQuantityPage"}});
 			var vm = BindingContext as UpdateParQuantityViewModel;
 			if (vm != null) {
 				await vm.OnAppearing ();
