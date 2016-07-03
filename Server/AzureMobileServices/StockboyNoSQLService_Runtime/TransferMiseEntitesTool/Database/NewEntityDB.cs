70aa9ea8ef94110125b08ea3c8032672d66d68b4
Mathieson Sterling <mathieson@misepos.com>
2016-07-02 22:42:40 -0400

    Added new NOSQL service

---
+++ Server/AzureMobileServices/StockboyNoSQLService_Runtime/TransferMiseEntitesTool/Database/NewEntityDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferMiseEntitesTool.Database
+{
+    class NewEntityDB : DbContext
+    {
+        public NewEntityDB() : base("name=MS_TableConnectionString")
+        {
+            
+        }
+
+        public virtual DbSet<AzureEntityStorage> AzureEntityStorages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AzureEntityStorage>()
+                .Property(e => e.Version).IsFixedLength();
+        }
+    }
+}
---
+++ Server/AzureMobileServices/StockboyNoSQLService_Runtime/TransferMiseEntitesTool/Producers/SimpleAzureEntityProducer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferMiseEntitesTool.Producers
+{
+    public class SimpleAzureEntityProducer : BaseAzureEntitiesProducer
+    {
+    }
+}
