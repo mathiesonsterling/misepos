5460bd1f7e72ca0580c599e6dce7113221a1d47c
Mathieson Sterling <mathieson@misepos.com>
2016-07-02 23:33:19 -0400

    Updated version

--- Client/Mise.Inventory/Mise.Inventory/Mise.Inventory.iOS/Main.cs
+++ Client/Mise.Inventory/Mise.Inventory/Mise.Inventory.iOS/Main.cs
@@ -12,6 +12,7 @@ namespace Mise.Inventory.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            Xamarin.Insights.Initialize ("ed66b318e3febcdfc08ca11d6c20e33c79f2f434");
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
